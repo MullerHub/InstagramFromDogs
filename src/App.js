e096e15984dd74e4bb7e2f55f3b8cd0b769af46c
murilo muller <murilomuller@protonmail.com>
2022-08-11 12:02:15 -0300

    Protected Routes finished

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
-import React from 'react';
+import React from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Header from './Components/Header';
-import Footer from './Components/Footer';
+import Header from './Components/Header'
+import Footer from './Components/Footer'
 import Home from './Components/Home'
-import Login from './Components/Login/Login';
-import User from './Components/Login/Login';
-import { UserStorage } from './UserContext';
+import Login from './Components/Login/Login'
+import User from './Components/User/User'
+import { UserStorage } from './UserContext'
+import ProtectedRoute from './Components/Helper/ProtectedRoute'
 
 function App() {
   return (
-    <div className='App'>
-    <BrowserRouter>
-    <UserStorage>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="login/*" element={<Login />} />
-      <Route path="conta/*" element={<User />} />
-    </Routes>
-    <Footer />
-    </UserStorage>
-    </BrowserRouter>
+    <div className="App">
+      <BrowserRouter>
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="login/*" element={<Login />} />
+            <Route
+              path="conta/*"
+              element={
+                <ProtectedRoute>
+                  <User />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+          <Footer />
+        </UserStorage>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/Components/Helper/ProtectedRoute.js
+++ src/Components/Helper/ProtectedRoute.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { UserContext } from '../../UserContext'
+import { Navigate } from 'react-router-dom'
 
-const ProtectedRoute = () => {
+const ProtectedRoute = ({ children }) => {
   const { login } = React.useContext(UserContext)
-  return <div></div>
+  return login ? children : <Navigate to="/login" />
 }
 
 export default ProtectedRoute
