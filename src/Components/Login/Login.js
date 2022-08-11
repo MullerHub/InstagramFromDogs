2cfd37f1f0a0f8ebab67b5f7d1d7a4ed4eb5c25d
murilo muller <murilomuller@protonmail.com>
2022-08-10 22:48:43 -0300

    login, Form e Background estilizados

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -5,22 +5,24 @@ import LoginForm from './LoginForm'
 import UserContext from '../../UserContext'
 import LoginPasswordLost from './LoginPasswordLost'
 import LoginPasswordReset from './LoginPasswordReset'
+import styles from './Login.module.css'
 
 const Login = () => {
-  const {login} = React.useContext(UserContext)
+  const { login } = React.useContext(UserContext)
 
-  if(login === true) return <Navigate to="/conta"/>
-  return <section>
-    <div>
-    <Routes>
-    <Route path='/' element={<LoginForm />} />
-    <Route path='criar' element={<LoginCreate />} />
-    <Route path='perdeu' element={<LoginPasswordLost />} />
-    <Route path='resetar' element={<LoginPasswordReset />} />
- 
-    </Routes>
-    </div>
-  </section>
+  if (login === true) return <Navigate to="/conta" />
+  return (
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="criar" element={<LoginCreate />} />
+          <Route path="perdeu" element={<LoginPasswordLost />} />
+          <Route path="resetar" element={<LoginPasswordReset />} />
+        </Routes>
+      </div>
+    </section>
+  )
 }
 
 export default Login
