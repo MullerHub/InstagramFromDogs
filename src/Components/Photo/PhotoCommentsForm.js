1ca5a9a5e9c805885b273e947c661688878d51df
murilo-notebook <murilomuller@protonmail.com>
2022-08-16 18:22:38 -0300

    Photo Delete finalizado

--- src/Components/Photo/PhotoCommentsForm.js
+++ src/Components/Photo/PhotoCommentsForm.js
@@ -3,31 +3,33 @@ import { ReactComponent as Enviar } from '../../Assets/enviar.svg'
 import { COMMENT_POST } from '../../Api'
 import useFetch from '../../Hooks/useFetch'
 import Error from '../Helper/Error'
+import styles from './PhotoCommentsForm.module.css'
 
-const PhotoCommentsForm = ({id, setComments}) => {
+const PhotoCommentsForm = ({ id, setComments }) => {
   const [comment, setComment] = React.useState('')
   const { request, error } = useFetch()
 
   async function handleSubmit(event) {
     event.preventDefault()
-    const { url, options } = COMMENT_POST(id, {comment})
-    const {response, json } = await request(url, options)
+    const { url, options } = COMMENT_POST(id, { comment })
+    const { response, json } = await request(url, options)
     if (response.ok) {
       setComment('')
-      setComments((comments) => [...comments, json])
+      setComments(comments => [...comments, json])
     }
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <textarea
+        className={styles.textarea}
         id="comment"
         name="comment"
         placeholder="comente..." // ajustar o plcaholder com uma label por cima do textarea/formulario
         value={comment}
         onChange={({ target }) => setComment(target.value)}
       />
-      <button>
+      <button className={styles.button}>
         <Enviar />
       </button>
       <Error error={error} />
