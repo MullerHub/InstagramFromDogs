016785a53965c76d792a143910ba5eec344e23bc
murilo-notebook <murilomuller@protonmail.com>
2022-08-16 14:35:43 -0300

    comentario POST concluido

--- src/Components/Photo/PhotoCommentsForm.js
+++ src/Components/Photo/PhotoCommentsForm.js
@@ -2,21 +2,24 @@ import React from 'react'
 import { ReactComponent as Enviar } from '../../Assets/enviar.svg'
 import { COMMENT_POST } from '../../Api'
 import useFetch from '../../Hooks/useFetch'
-import styles from './PhotoComments.module.css '
+import Error from '../Helper/Error'
 
-
-const PhotoCommentsForm = ({id}) => {
+const PhotoCommentsForm = ({id, setComments}) => {
   const [comment, setComment] = React.useState('')
   const { request, error } = useFetch()
 
-  async function handlesubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault()
-    const { url, option } = COMMENT_POST(id, {comment})
-    await request(url, option)
+    const { url, options } = COMMENT_POST(id, {comment})
+    const {response, json } = await request(url, options)
+    if (response.ok) {
+      setComment('')
+      setComments((comments) => [...comments, json])
+    }
   }
 
   return (
-    <form onSubmit={handlesubmit}>
+    <form onSubmit={handleSubmit}>
       <textarea
         id="comment"
         name="comment"
@@ -27,6 +30,7 @@ const PhotoCommentsForm = ({id}) => {
       <button>
         <Enviar />
       </button>
+      <Error error={error} />
     </form>
   )
 }
