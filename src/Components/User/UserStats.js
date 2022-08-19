152ae59f620d26b959cac9e80bbe4f2a95d929a0
murilo muller <murilomuller@protonmail.com>
2022-08-19 13:37:27 -0300

    graphs finished

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,7 +1,32 @@
 import React from 'react'
+import useFetch from '../../Hooks/useFetch'
+import Loading from '../Helper/Loading'
+import Error from '../Helper/Error'
+import Head from '../Helper/Head'
+import UseStatsGraphs from './UseStatsGraphs'
+import { GET_STATS } from '../../Api'
 
 const UserStats = () => {
-  return <div>UserStats</div>
+  const { data, error, loading, request } = useFetch()
+  React.useEffect(() => {
+    async function getData() {
+      const { url, options } = GET_STATS()
+      await request(url, options)
+    }
+    getData()
+  }, [request])
+
+  if (loading) return <Loading />
+  if (error) return <Error error={error} />
+
+  if (data)
+    return (
+      <div>
+        <Head title="Estatísticas" />
+        <UseStatsGraphs data={data} />
+      </div>
+    )
+  else return null
 }
 
 export default UserStats
